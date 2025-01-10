2e4ba723442cfe4c4019366bc1e5508a7667ad91
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-01-10 12:46:48 +0100

    initial Go files

--- cmd/plugin/main.go
+++ cmd/plugin/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/mongodb-labs/atlas-cli-plugin-terraform/internal/cli/hello"
+	"github.com/spf13/cobra"
+)
 
 func main() {
-	fmt.Println("WIP - Placeholder for CLI plugin entrypoint")
+	terraformCmd := &cobra.Command{
+		Use:   "terraform",
+		Short: "Root command of the Atlas CLI plugin for MongoDB Atlas Provider",
+	}
+
+	terraformCmd.AddCommand(
+		hello.Builder(),
+	)
+
+	completionOption := &cobra.CompletionOptions{
+		DisableDefaultCmd:   true,
+		DisableNoDescFlag:   true,
+		DisableDescriptions: true,
+		HiddenDefaultCmd:    true,
+	}
+	rootCmd := &cobra.Command{
+		Aliases:            []string{"tf"},
+		DisableFlagParsing: true,
+		DisableAutoGenTag:  true,
+		DisableSuggestions: true,
+		CompletionOptions:  *completionOption,
+	}
+	rootCmd.AddCommand(terraformCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
