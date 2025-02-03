9657eb2ec89d0be6a6f900da48479c3cfa376849
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-02-03 19:40:57 +0100

    comments

--- internal/hcl/testdata/clu2adv/free_cluster_with_count.in.tf
+++ internal/hcl/testdata/clu2adv/free_cluster_with_count.in.tf
@@ -2,11 +2,12 @@ resource "resource1" "res1" {
   name = "name1"
 }
 
-resource "mongodbatlas_cluster" "free_cluster" {
+resource "mongodbatlas_cluster" "free_cluster" { # comment in the resource
+  # comment in own line
   count                       = local.use_free_cluster ? 1 : 0
-  project_id                  = var.project_id
+  project_id                  = var.project_id # inline comment kept
   name                        = var.cluster_name
-  provider_name               = "TENANT"
+  provider_name               = "TENANT" # inline comment for attribute moved is not kept
   backing_provider_name       = "AWS"
   provider_region_name        = var.region
   provider_instance_size_name = "M0"
--- internal/hcl/testdata/clu2adv/free_cluster_with_count.out.tf
+++ internal/hcl/testdata/clu2adv/free_cluster_with_count.out.tf
@@ -2,9 +2,10 @@ resource "resource1" "res1" {
   name = "name1"
 }
 
-resource "mongodbatlas_advanced_cluster" "free_cluster" {
+resource "mongodbatlas_advanced_cluster" "free_cluster" { # comment in the resource
+  # comment in own line
   count        = local.use_free_cluster ? 1 : 0
-  project_id   = var.project_id
+  project_id   = var.project_id # inline comment kept
   name         = var.cluster_name
   cluster_type = "REPLICASET"
   replication_specs = [{
