2e9de57441847ef1c870d1812bfe51ee6eebbce4
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-02-03 18:50:26 +0100

    pass tests

--- internal/hcl/testdata/clu2adv/free_cluster_with_count.in.tf
+++ internal/hcl/testdata/clu2adv/free_cluster_with_count.in.tf
@@ -1,4 +1,8 @@
-resource "mongodbatlas_cluster" "project_cluster_free" {
+resource "resource1" "res1" {
+  name = "name1"
+}
+
+resource "mongodbatlas_cluster" "free_cluster" {
   count                       = local.use_free_cluster ? 1 : 0
   project_id                  = var.project_id
   name                        = var.cluster_name
@@ -6,4 +10,8 @@ resource "mongodbatlas_cluster" "project_cluster_free" {
   backing_provider_name       = "AWS"
   provider_region_name        = var.region
   provider_instance_size_name = "M0"
+}
+
+data "mongodbatlas_cluster" "cluster2" {
+  name = "name4"
 }
