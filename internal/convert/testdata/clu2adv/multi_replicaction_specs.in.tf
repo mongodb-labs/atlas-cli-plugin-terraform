57f284ba77d4b7940de34a5f91f9d36682a5e13f
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-02-17 13:02:17 +0100

    failing test

---
+++ internal/convert/testdata/clu2adv/multi_replicaction_specs.in.tf
@@ -0,0 +1,29 @@
+resource "mongodbatlas_cluster" "multirep" {
+  project_id                  = "66d979971ec97b7de1ef8777"
+  name                        = "multirep"
+  disk_size_gb                = 80
+  num_shards                  = 1
+  cloud_backup                = false
+  cluster_type                = "GEOSHARDED"
+  provider_name               = "AWS"
+  provider_instance_size_name = "M10"
+
+  replication_specs {
+    zone_name  = "Zone 1"
+    num_shards = 1
+    regions_config {
+      region_name     = "US_EAST_1"
+      electable_nodes = 3
+      priority        = 7
+    }
+  }
+  replication_specs {
+    zone_name  = "Zone 2"
+    num_shards = 1
+    regions_config {
+      region_name     = "US_WEST_2"
+      electable_nodes = 3
+      priority        = 7
+    }
+  }
+}
