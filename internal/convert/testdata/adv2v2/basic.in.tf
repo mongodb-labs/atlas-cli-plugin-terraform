c0f48cb9441b096a7407b90441f154f774b4643d
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-08-14 12:51:05 +0200

    basic test (at the moment will fail)

--- internal/convert/testdata/adv2v2/basic.in.tf
+++ internal/convert/testdata/adv2v2/basic.in.tf
@@ -1,4 +1,16 @@
-resource "mongodbatlas_advanced_cluster" "basic" {
-  name = "basic"
-  // TODO: missing fields as transformation is not implemented yet
+resource "mongodbatlas_advanced_cluster" "clu" {
+  project_id   = "66d979971ec97b7de1ef8777"
+  name         = "clu"
+  cluster_type = "REPLICASET"
+  replication_specs {
+    region_configs {
+      priority      = 7
+      provider_name = "AWS"
+      region_name   = "EU_WEST_1"
+      electable_specs {
+        instance_size = "M10"
+        node_count    = 3
+      }
+    }
+  }
 }
