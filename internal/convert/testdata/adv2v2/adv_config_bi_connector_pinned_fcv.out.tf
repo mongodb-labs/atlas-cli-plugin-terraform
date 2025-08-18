ffff1c82515172253b89f90c8a6fd92c19434030
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-08-18 10:38:10 +0200

    advanced_configuration, bi_connector and pinned_fvc tests

---
+++ internal/convert/testdata/adv2v2/adv_config_bi_connector_pinned_fcv.out.tf
@@ -0,0 +1,38 @@
+resource "mongodbatlas_advanced_cluster" "this" {
+  project_id   = var.project_id
+  name         = var.cluster_name
+  cluster_type = "REPLICASET"
+
+
+
+  replication_specs = [
+    {
+      region_configs = [
+        {
+          priority      = 7
+          provider_name = "AWS"
+          region_name   = "EU_WEST_1"
+          electable_specs = {
+            instance_size = "M10"
+            node_count    = 3
+          }
+        }
+      ]
+    }
+  ]
+  advanced_configuration = {
+    # comments in advanced_configuration are kept
+    javascript_enabled = true
+  }
+  bi_connector_config = {
+    # comments in bi_connector_config are kept
+    enabled         = true
+    read_preference = "secondary"
+  }
+  pinned_fcv = {
+    # comments in pinned_fcv are kept
+    expiration_date = var.fcv_date
+  }
+
+  # Updated by atlas-cli-plugin-terraform, please review the changes.
+}
