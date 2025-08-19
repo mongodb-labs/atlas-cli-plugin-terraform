8e713dc9c4df6d3b97a66d52b1560e07a4f7cd41
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-08-19 10:50:59 +0200

    failing multi_config test

---
+++ internal/convert/testdata/adv2v2/multi_region.out.tf
@@ -0,0 +1,45 @@
+resource "mongodbatlas_advanced_cluster" "multi_region" {
+  project_id     = var.project_id
+  name           = "cluster-multi-region"
+  cluster_type   = "REPLICASET"
+  backup_enabled = true
+
+  replication_specs = [
+    {
+      region_configs = [
+        {
+          provider_name = "AWS"
+          region_name   = "US_EAST_1"
+          priority      = 7
+          electable_specs = {
+            node_count    = 3
+            instance_size = "M10"
+            disk_size_gb  = 100
+          }
+        },
+        {
+          provider_name = "AWS"
+          region_name   = "US_WEST_2"
+          priority      = 6
+          electable_specs = {
+            node_count    = 3
+            instance_size = "M10"
+            disk_size_gb  = 100
+          }
+        },
+        {
+          provider_name = "AWS"
+          region_name   = "US_WEST_1"
+          priority      = 5
+          electable_specs = {
+            node_count    = 1
+            instance_size = "M10"
+            disk_size_gb  = 100
+          }
+        }
+      ]
+    }
+  ]
+
+  # Updated by atlas-cli-plugin-terraform, please review the changes.
+}
