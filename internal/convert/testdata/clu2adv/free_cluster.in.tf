48eb5d056cf2342fe2aa9e990f2ce398bc505d38
Leo Antoli <430982+lantoli@users.noreply.github.com>
2025-09-11 16:43:59 +0200

    failing tests with feature to implement

--- internal/convert/testdata/clu2adv/free_cluster.in.tf
+++ internal/convert/testdata/clu2adv/free_cluster.in.tf
@@ -17,6 +17,26 @@ resource "mongodbatlas_cluster" "count" {
   provider_instance_size_name = "M0"
 }
 
+
+resource "mongodbatlas_cluster" "upgrade_from_free_cluster" {
+  # upgraded free cluster to dedicated
+  project_id                  = var.project_id
+  name                        = var.cluster_name
+  provider_name               = "AWS" # changed from TENANT to AWS
+  provider_instance_size_name = "M10" # changed from M0 to M10
+  # removed backing_provider_name = AWS"
+  provider_region_name = var.region
+}
+
+resource "mongodbatlas_cluster" "upgrade_from_free_cluster_with_variables" {
+  # upgraded free cluster to dedicated, using variables in all attributes
+  project_id                  = var.project_id
+  name                        = var.cluster_name
+  provider_name               = var.provider_name
+  provider_instance_size_name = var.instance_size
+  provider_region_name        = var.region
+}
+
 resource "another_resource" "res1" {
   name = "name1"
 }
